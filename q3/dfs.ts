b3d2a76859fce56cd2bfa15ad8361a752e61336e
Narong Pattanayanon <superhuiman@gmail.com>
2023-05-07 23:05:50 +0700

    Update dfs.ts

--- q3/dfs.ts
+++ q3/dfs.ts
@@ -1,93 +1,88 @@
-class TreeNode<T> {
-  value: T;
-  left: TreeNode<T> | null;
-  right: TreeNode<T> | null;
+class TreeNode {
+  value: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
 
-  constructor(value: T) {
+  constructor(value: number) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
-class Tree<T> {
-  root: TreeNode<T> | null;
+class Tree {
+  root: TreeNode | null;
 
   constructor() {
     this.root = null;
   }
 
-  static dfsFactory<T>(method: "preorder" | "inorder" | "postorder"): DFS<T> {
+  static dfsFactory(method: "preorder" | "inorder" | "postorder") {
     switch (method) {
       case "preorder":
-        return new PreorderDFS<T>();
+        return new PreorderDFS();
       case "inorder":
-        return new InorderDFS<T>();
+        return new InorderDFS();
       case "postorder":
-        return new PostorderDFS<T>();
+        return new PostorderDFS();
       default:
         throw new Error("Invalid traversal method specified");
     }
   }
 }
 
-interface DFS<T> {
-  traverse(root: TreeNode<T>): T[];
-}
-
-// Define the PreorderDFS class
-class PreorderDFS<T> implements DFS<T> {
-  traverse(root: TreeNode<T>): T[] {
-    const result: T[] = [];
+class PreorderDFS {
+  execute(root: TreeNode): number[] {
+    const result: number[] = [];
     if (root !== null) {
       result.push(root.value);
-      result.push(...this.traverse(root.left!));
-      result.push(...this.traverse(root.right!));
+      result.push(...this.execute(root.left!));
+      result.push(...this.execute(root.right!));
     }
     return result;
   }
 }
 
-class InorderDFS<T> implements DFS<T> {
-  traverse(root: TreeNode<T>): T[] {
-    const result: T[] = [];
+class InorderDFS {
+  execute(root: TreeNode): number[] {
+    const result: number[] = [];
     if (root !== null) {
-      result.push(...this.traverse(root.left!));
+      result.push(...this.execute(root.left!));
       result.push(root.value);
-      result.push(...this.traverse(root.right!));
+      result.push(...this.execute(root.right!));
     }
     return result;
   }
 }
 
-class PostorderDFS<T> implements DFS<T> {
-  traverse(root: TreeNode<T>): T[] {
-    const result: T[] = [];
+class PostorderDFS {
+  execute(root: TreeNode): number[] {
+    const result: number[] = [];
     if (root !== null) {
-      result.push(...this.traverse(root.left!));
-      result.push(...this.traverse(root.right!));
+      result.push(...this.execute(root.left!));
+      result.push(...this.execute(root.right!));
       result.push(root.value);
     }
     return result;
   }
 }
 
-// Example usage
-const tree = new Tree<number>();
+const tree = new Tree();
 tree.root = new TreeNode(1);
 tree.root.left = new TreeNode(2);
 tree.root.right = new TreeNode(3);
 tree.root.left.left = new TreeNode(4);
 tree.root.left.right = new TreeNode(5);
-tree.root.right.left = new TreeNode(6);
-tree.root.right.right = new TreeNode(7);
+tree.root.left.right.left = new TreeNode(6);
+tree.root.left.right.right = new TreeNode(7);
+tree.root.left.right.right.left = new TreeNode(8);
 
 const dfs_pre = Tree.dfsFactory("preorder");
-const result_pre = dfs_pre.traverse(tree.root);
+const result_pre = dfs_pre.execute(tree.root);
 console.log(result_pre);
 const dfs_post = Tree.dfsFactory("postorder");
-const result_post = dfs_post.traverse(tree.root);
+const result_post = dfs_post.execute(tree.root);
 console.log(result_post);
 const dfs_in = Tree.dfsFactory("inorder");
-const result_in = dfs_in.traverse(tree.root);
+const result_in = dfs_in.execute(tree.root);
 console.log(result_in);
